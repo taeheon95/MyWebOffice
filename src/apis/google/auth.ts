6ee598983639810e685ba60b55e13e15bb5b5121
taeheon <dalo952003@gmail.com>
2022-05-03 23:16:55 +0900

    구글 토큰 관련 함수 추가

--- src/apis/google/auth.ts
+++ src/apis/google/auth.ts
@@ -1,3 +1,46 @@
-const url = "https://www.google.com/oauth2/v4/token";
+import { GoogleAuth } from "../../types/auth";
 
-export default url;
+const url = "https://oauth2.googleapis.com/token";
+const client_id = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const client_secret = import.meta.env.VITE_GOOGLE_CLIENT_SECRET;
+
+const getGoogleAccessTokens = async (refresh_token: string) => {
+  const response = await fetch(url, {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify({
+      grant_type: "refresh_token",
+      refresh_token,
+      client_secret,
+      client_id,
+    }),
+  });
+  if (response.ok) {
+    return (await response.json()) as GoogleAuth;
+  } else {
+    console.error(response);
+    throw new Error(response.statusText);
+  }
+};
+
+const getGoogleTokens = async (code: string, service: string) => {
+  const response = await fetch(url, {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify({
+      redirect_uri: `https://localhost:3000/redirect/${service}`,
+      grant_type: "access",
+      client_secret,
+      client_id,
+      code,
+    }),
+  });
+  if (response.ok) {
+    return (await response.json()) as GoogleAuth;
+  } else {
+    console.error(response);
+    throw new Error(response.statusText);
+  }
+};
+
+export { getGoogleTokens, getGoogleAccessTokens };
